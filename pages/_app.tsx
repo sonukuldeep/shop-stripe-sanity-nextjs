5d30858751bd2e53234c6186bb8ed4f50ba28e26
sonukuldeep <sonukuldeep@outlook.com>
2023-04-01 17:08:52 +0530

    typescript bug fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import type { AppProps } from 'next/app.js';
 import { Toaster } from 'react-hot-toast';
 import DataState from "../context/DataState.js"
 import { Layout } from '../components';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
       <DataState>
         <Layout>
--- pages/cancel.jsx
+++ pages/cancel.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const cancel = () => {
     return (
         <>
-            <div>Something when wrong</div>
+            <div>Something when wrong... If you were charged and the transaction failed, it will be refunded in 5 working days</div>
             <Link href="/">
                 <button type="button" width="100px" className='btn'>Go back home</button>
             </Link>
